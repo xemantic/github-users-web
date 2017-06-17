3e7f0c8b49bbdcf53186c458ecf84e5f26ed4c3d
Kazik Pogoda <morisil@xemantic.com>
2017-06-17 19:50:29 +0200

    soyutils_idom from closure-templates and templates.soy moved to the new location in the server module

--- src/main/java/com/xemantic/githubusers/public/README.md
+++
@@ -1,5 +0,0 @@
-# Why folder public is in src/main/java and not inside src/main/resources?
-
-It's according to `net.ltgt.gwt.maven:gwt-maven-plugin` examples.
-Configuring it this way allows to see changes in HTML quickly in the
-Super Dev Mode after recompiling the `GitHubUsersWeb` module.
--- src/main/java/com/xemantic/githubusers/public/index.js
+++
@@ -1,7 +0,0 @@
-/* menu */
-var drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
-document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
-
-/* it will center the grid and make it react to window resizing */
-
-mdc.gridList.MDCGridList.attachTo(document.querySelector(".mdc-grid-list"));
